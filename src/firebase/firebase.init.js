9c9ce5b32046fb40bcad012413cfffb391f71938
nosiba28 <jucse28.353@gmail.com>
2023-11-03 20:32:48 +0600

    update modal added

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDz1V_chBsqS8GivdQkhJuttw0fodXji-E",
-  authDomain: "nsbgram.firebaseapp.com",
-  projectId: "nsbgram",
-  storageBucket: "nsbgram.appspot.com",
-  messagingSenderId: "1056564196629",
-  appId: "1:1056564196629:web:39eb2f248bbafdd66ba51c",
-  measurementId: "G-79QZPSCCMR"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+  measurementId: import.meta.env.VITE_measurementId
 };
 
 // Initialize Firebase
